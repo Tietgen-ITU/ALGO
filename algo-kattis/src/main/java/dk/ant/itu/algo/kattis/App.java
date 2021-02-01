43e1e7cdea8baa411aa080211e52339617788b0b
Andreas Tietgen <andreotietgen@hotmail.com>
2021-02-01 22:04:17 +0100

    Init

---
+++ algo-kattis/src/main/java/dk/ant/itu/algo/kattis/App.java
@@ -0,0 +1,13 @@
+package dk.ant.itu.algo.kattis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ algo-training/src/main/java/dk/ant/itu/algo/training/App.java
@@ -0,0 +1,13 @@
+package dk.ant.itu.algo.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
